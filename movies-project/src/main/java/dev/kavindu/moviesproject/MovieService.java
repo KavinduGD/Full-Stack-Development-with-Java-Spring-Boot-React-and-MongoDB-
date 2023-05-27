c105162ca3d0f66e414397599c1d53e4fced4294
it21038914 <it21038914@my.sliit.lk>
2023-05-27 18:47:35 +0530

    Movie Backend completed

--- movies-project/src/main/java/dev/kavindu/moviesproject/MovieService.java
+++ movies-project/src/main/java/dev/kavindu/moviesproject/MovieService.java
@@ -1,15 +1,29 @@
 package dev.kavindu.moviesproject;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
+
     @Autowired
     private MovieRepository  movieRepository;
+
+
     public List<Movie> allMovies(){
         return movieRepository.findAll();
     }
+
+//    public Optional<Movie> singleMovie(ObjectId id){
+//        return movieRepository.findById(id);
+//    }
+
+    public Optional<Movie> singleMovieFindByImdbId(String imdbId){
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+
 }
