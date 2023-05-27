0ee9c6cf15ad60cbfac0720c0687989a90d19829
it21038914 <it21038914@my.sliit.lk>
2023-05-27 11:08:16 +0530

    1st get API

---
+++ movies-project/src/test/java/dev/kavindu/moviesproject/MoviesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kavindu.moviesproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
