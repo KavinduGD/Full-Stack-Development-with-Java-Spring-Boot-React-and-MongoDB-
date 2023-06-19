3fd2048be4e5c508b72c3a75018bbfb2639ce838
it21038914 <it21038914@my.sliit.lk>
2023-06-19 23:55:15 +0530

    fixing corse error

--- movies-project/src/main/java/dev/kavindu/moviesproject/ReviewController.java
+++ movies-project/src/main/java/dev/kavindu/moviesproject/ReviewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://vedio-stream.vercel.app/")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
